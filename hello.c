9714a43d384ba0b87cbcdd691232930da4ab1b6b
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-20 09:08:30 +0000

    Wed, Sep 20, 2023, 2:38 PM +05:30

--- hello.c
+++ hello.c
@@ -1,1 +1,8 @@
-#include <
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    string first=get_string("What's your name? ");
+    string second=get_string("What's your age? " );
+    printf(" Hello,%s. You're %s? that's awesome. \n",first,second );
+}
