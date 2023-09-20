00c656204b0f5d2c779b4691ca410eef9cc4d268
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-20 11:50:34 +0000

    Wed, Sep 20, 2023, 5:20 PM +05:30

--- accept.c
+++ accept.c
@@ -4,12 +4,12 @@
 int main(void)
 {
     char c= get_char("Do you agree with this?");
-    if (c=='y',c=='Y')
+    if (c=='y'c=='Y')
     {
-        printf("Agreed.");
+        printf("Agreed\n.");
     }
-    else if(c=='n',c=='N')
+    else if(c=='n' c=='N')
     {
-        printf("Not agreed.");
+        printf("Not agreed\n.");
     }
 }
