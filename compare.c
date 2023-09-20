f182cbf39fdb8dcde45a1a0b5aa7148bf994f517
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-20 09:47:53 +0000

    Wed, Sep 20, 2023, 3:17 PM +05:30

--- compare.c
+++ compare.c
@@ -8,14 +8,14 @@ int a=get_int("Input a number ");
 int b=get_int("Input another number");
 
 if (a>b){
-    printf("%i>%i ",a,b);
+    printf("%i>%i \n",a,b);
 
 }
 else if(a<b){
-     printf("%i<%i",a,b);
+     printf("%i<%i \n",a,b);
      }
     else{
-        printf("%i=%i",a,b);
+        printf("%i=%i \n",a,b);
 
     }
     }
