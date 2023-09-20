73edfb4c71a2da933978966802af7777d244c6f2
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-20 11:47:34 +0000

    Wed, Sep 20, 2023, 5:17 PM +05:30

---
+++ accept.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c=;
+    if (c=='y' or c=='Y')
+    {
+        printf("Agreed.");
+    }
+    else if(c=='n' or c=='N')
+    {
+        printf("Not agreed.");
+    }
+}
