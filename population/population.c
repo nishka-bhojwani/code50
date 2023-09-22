00ee828a3a2b0c264242924f5d9bc7041390de6f
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-22 18:00:51 +0000

    Fri, Sep 22, 2023, 11:30 PM +05:30

--- population/population.c
+++ population/population.c
@@ -6,30 +6,24 @@ int main(void)
     // TODO: Prompt for start size
     int n;
     int p;
-     do
-     {
-        n=get_int("Start size: ");
-
-     }
-     while(n<9);
-
-     do
-     {
-        p=get_int("End size: ");
-
-     }
-    while(p<n);
-    int y=0;
-
-    while(n<p)
+    do
     {
-        n= n+(n/3)-(n/4);
-        y=y+1;
+        n = get_int("Start size: ");
+    }
+    while (n < 9);
 
+    do
+    {
+        p = get_int("End size: ");
+    }
+    while (p < n);
+    int y = 0;
 
+    while (n < p)
+    {
+        n = n + (n / 3) - (n / 4);
+        y = y + 1;
     }
 
-    printf("Years: %i\n",y);
-
-
+    printf("Years: %i\n", y);
 }
