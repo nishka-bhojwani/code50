60f4e81f440013b67706916b46988714744e6438
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-22 19:00:22 +0000

    Sat, Sep 23, 2023, 12:30 AM +05:30

--- lecture_1/phonebook.c
+++ lecture_1/phonebook.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    int age = get_int("What's your age ? ");
+    string number = get_string("What's your phone number ? ");
+
+    printf("\n" );
+    printf("Name: %s\n" , "Age: %i\n" , "Number: %s\n", name , age , number);
+    
+
+
 
 
 }
+
+
+
+
+
