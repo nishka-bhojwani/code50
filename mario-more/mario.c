98f592532579665f9c7824ba325b34a99929f3e0
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-23 19:04:03 +0000

    Sun, Sep 24, 2023, 12:34 AM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,32 +4,31 @@
 int main(void)
 {
     int h;
-    //getting the height from the user and storing it in a variable
-    //prompts user again incase of an input greater than 8 or smaller than 1
+    // getting the height from the user and storing it in a variable
+    // prompts user again incase of an input greater than 8 or smaller than 1
     do
     {
         h = get_int("Height: ");
-     }
-    while ( h < 1 || h > 8);
+    }
+    while (h < 1 || h > 8);
 
-    //using nested for loops for specifying the required output line by line
-    for(int i=1; i<=h ; i++)
+    // using nested for loops for specifying the required output line by line
+    for (int i = 1; i <= h; i++)
     {
-         for(int j=i ; j<=h-1; j++)
+        for (int j = i; j <= h - 1; j++)
         {
-            printf(" ");//prints the required spaces (h-1) in this case
+            printf(" "); // prints the required spaces (h-1) in this case
         }
-        for(int m=1;m<=i; m++)
+        for (int m = 1; m <= i; m++)
         {
-            printf("#"); //prints number of #'s required that is equal to the roq number i.e. i
+            printf("#"); // prints number of #'s required that is equal to the roq number i.e. i
         }
-             printf(" "" ");// prints the gap in the middle
-        for(int n=1;n<=i;n++) // this for loop prints the hash triangle on the right hand side
+        printf(" "
+               " ");                 // prints the gap in the middle
+        for (int n = 1; n <= i; n++) // this for loop prints the hash triangle on the right hand side
         {
             printf("#");
         }
-              printf("\n");
-     }
+        printf("\n");
     }
-
-
+}
