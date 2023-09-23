4afc1891faf50f48a7746254b3b81343a72b6081
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-23 13:23:15 +0000

    Sat, Sep 23, 2023, 6:53 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+         height = get_int("Height of pyramid: /n");
+    }
+    while(height<1,height>8);
+
 }
