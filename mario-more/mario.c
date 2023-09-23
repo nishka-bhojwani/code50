c040b9b4e5069140a03f171856cf356a052e134c
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-23 15:32:50 +0000

    Sat, Sep 23, 2023, 9:02 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,6 +4,9 @@
 int main(void)
 {
     int h;
+    int i;
+    int j;
+    
     do
     {
         h = get_int("Height: ");
@@ -15,8 +18,13 @@ int main(void)
     {
         for(j=1;j<=h;j++)
         {
-            printf(")
+            printf("(8-j)*'',#*j,2*'',#*j/n");
+            j=j+1;
+            i=i+1;
+
+
         }
     }
+
 
 }
