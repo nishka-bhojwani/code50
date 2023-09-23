88af625fdd7379a87f06d0eb6788cb59161fdff1
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-23 18:55:17 +0000

    Sun, Sep 24, 2023, 12:25 AM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,35 +4,32 @@
 int main(void)
 {
     int h;
-
-
+    //getting the height from the user and storing it in a variable
+    //prompts user again incase of an input greater than 8 or smaller than 1
     do
     {
         h = get_int("Height: ");
-
-    }
+     }
     while ( h < 1 || h > 8);
 
-    for(int i=1; i<=h ;i++)
+    //using nested for loops for specifying the required output line by line
+    for(int i=1; i<=h ; i++)
     {
-
-        for(int j=i;j<=h-1;j++)
+         for(int j=i ; j<=h-1; j++)
         {
-            printf(" ");
-
+            printf(" ");//prints the required spaces (h-1) in this case
         }
         for(int m=1;m<=i; m++)
         {
             printf("#");
         }
-         printf(" "" ");
+             printf(" "" ");
         for(int n=1;n<=i;n++)
         {
             printf("#");
         }
               printf("\n");
      }
-
     }
 
 
