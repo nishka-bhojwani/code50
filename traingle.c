3bafe11c63661bc778210fd6b5da41acabc73187
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-26 11:37:26 +0000

    Tue, Sep 26, 2023, 5:07 PM +05:30

--- traingle.c
+++ traingle.c
@@ -1,4 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float multipy(float a,float b);
+float valid_traingle(float side1, float side2 , float side3);
+
+int main(void)
+{
+    int x = get_int("Side1 : ");
+    int y = get_int("Side2 : ");
+    int z = get_int("Side 3 : ");
+
+    
+
+
+
+}
