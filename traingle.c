790affc4d40d56f992775b1f1acb377a7f3fee0a
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-26 17:56:02 +0000

    Tue, Sep 26, 2023, 11:26 PM +05:30

--- traingle.c
+++ traingle.c
@@ -5,9 +5,12 @@ bool valid_triangle(float side1, float side2 , float side3);
 
 int main(void)
 {
+    do{
     float x = get_float("Side1 : ");
     float y = get_float("Side2 : ");
     float z = get_float("Side 3 : ");
+    }
+    while(x<0 && y<0 && z<0);
 
     int triangle= valid_triangle(x,y,z);
     if (triangle==true)
@@ -21,20 +24,20 @@ int main(void)
 
 }
 
-bool valid_triangle(x,y,z)
+bool valid_triangle(float side1, float side2 , float side3)
 {
-    float sum1=x+y;
-    float sum2=y+z;
-    float sum3=x+z;
+    float sum1=side1+side2;
+    float sum2=side2+side3;
+    float sum3=side3+side1;
 
-    if (sum1>z , sum2>x, sum3>y){
-        bool traingle = true ;
-        return traingle;
+    if (sum1>side3, sum2>side1, sum3>side2)
+    {
+        return true;
     }
-    else{
-        bool triagle = false;
-        return traingle;
 
+    else
+    {
+        return false;
     }
 
 }
