d57a1322dcaffee23e5256b998848923dd766515
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-27 15:30:49 +0000

    Wed, Sep 27, 2023, 9:00 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -16,14 +16,5 @@ int main(void)
      card_number= get_long("Card Number: ");
      rem1= (int) (card_number/1e12);
      rem2= (int) (card_number/1e16);
-    printf("%i\n  %i\n" , rem1 , rem2 );
 
-
-
-    //int card_type = checksum(x);
-    //if (x==true){
-      //  if ((card_number/2)==7){
-        //}
-
-    //}
 }
