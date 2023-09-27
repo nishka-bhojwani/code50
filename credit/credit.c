206495bf6c5810a586ecff569fe0f820d38f32cb
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-27 08:51:59 +0000

    Wed, Sep 27, 2023, 2:21 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,9 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int checksum(float number);
+int checksum(long number);
 
 int main(void)
 {
-    //getting input from user 
+    //getting input from user
+    int x;
+    long card_number;
+    do {
+    long card_number= get_float("Card Number: ");
+    }
+    while()
+    int card_type = checksum(x);
+    if (x==true){
+        if ()
+    }
 }
