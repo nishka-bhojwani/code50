53ba51be54ff6b440f9834ec2e029bd6bd6cd750
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-27 18:05:49 +0000

    Wed, Sep 27, 2023, 11:35 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int checksum(long number );
+bool check_sum(long number );
 
 int main(void)
 {
@@ -18,11 +18,19 @@ int main(void)
      while(((int) (card_number/1e12)) <=0 || ((int) (card_number/1e16)>0)) ;
 
      string digits= (string) card_number;
-
+        bool check= check_sum(card_number);
+        if(check==true)
+        {
+            printf("VALID");
+        }
+        else
+        {
+            printf("INVALID");
+        }
 }
+4003600000000014
 
-
-int check_sum(long number)
+bool check_sum(long number)
 {
     int sum1=0;
     int sum2=0;
@@ -34,12 +42,25 @@ int check_sum(long number)
         sum1 = sum1+ ((int) dig[i])*2;
         i=i+2;
     }
-    return sum1;
 
-    for(int i= 0 ; i<n; )
+    for(int i= 0 ; i<n ; i=i+2 )
     {
-
+        sum2= sum2 + ((int) dig[i]);
+        i=i+2;
     }
+
+    int sum= sum1+sum2;
+    return sum;
+
+    if((sum/10)>0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 
 
 }
