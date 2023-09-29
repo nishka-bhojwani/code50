b7645baf7f1ead0c8dcca62261309bf6f621d132
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-09-29 18:14:36 +0000

    Fri, Sep 29, 2023, 11:44 PM +05:30

--- trial.c
+++ trial.c
@@ -4,24 +4,24 @@
 int main(void)
 {
     long num= get_long("Give me a credit card number, please :");
-    printf("%i", (int)(num/1e12));
+    //printf("%i", (int)(num/1e12));
 
-//  if (((int) num/1e12)==4 || ((int) num/1e13)== 4 || ((int) num/1e14)==4 || ((int)num/1e15)==4)
-   // {
-     //   printf("this cc could be a visa \n");
-    //}
-    //else if (((int)num/1e13)==34 || ((int)num/1e13)==37)
-    //{
-      //  printf("this could be an american express card \n" );
-    //}
-    //else if( ((int)num/1e14)==51 || ((int)num/1e14)==52 || (int)(num/1e14)==53 ||(num/1e14)==54 || (num/1e14)==55)
-    //{
-      //  printf("This could be a master card");
-    //}
-    //else
-    //{
-      //  printf("INVALID card");
+if ((int) (num/1e12)==4 || (int) (num/1e13)== 4 || (int) (num/1e14)==4 || (int)(num/1e15)==4)
+{
+  printf("this cc could be a visa \n");
+}
+else if ((int)(num/1e13)==34 || (int)(num/1e13)==37)
+    {
+      printf("this could be an american express card \n" );
+    }
+    else if( (int)(num/1e14)==51 || (int)(num/1e14)==52 || (int)(num/1e14)==53 ||(int)(num/1e14)==54 || (int)(num/1e14)==55)
+    {
+    printf("This could be a master card");
+    }
+    else
+    {
+    printf("INVALID card");
 
-    //}
+    }
 
 }
