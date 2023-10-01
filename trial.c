391db218a6c72ec87cbb05a61e2e460cc7b988a3
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-01 10:54:48 +0000

    Sun, Oct 1, 2023, 4:24 PM +05:30

--- trial.c
+++ trial.c
@@ -13,26 +13,26 @@ int main(void)
   {
     if ((int) (num/1e12)==4 || (int) (num/1e13)== 4 || (int) (num/1e14)==4 || (int)(num/1e15)==4)
   {
-  printf("this cc could be a visa \n");
+  printf("VISA \n");
   }
     else if ((int)(num/1e13)==34 || (int)(num/1e13)==37)
     {
-      printf("this could be an american express card \n" );
+      printf("AMERICAN EXPRESS\n" );
     }
     else if( (int)(num/1e14)==51 || (int)(num/1e14)==52 || (int)(num/1e14)==53 ||(int)(num/1e14)==54 || (int)(num/1e14)==55)
     {
 
-    printf("This could be a master card");
+    printf("MASTERCARD");
     }
     else
     {
-    printf("INVALID card");
+    printf("INVALID");
 
     }
 }
   else
   {
-    printf("INVALID CARD");
+    printf("INVALID");
   }
  printf("\n");
 
