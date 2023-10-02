ad9a55be427ecb9668299faa81991a762a05465e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-02 17:17:21 +0000

    Mon, Oct 2, 2023, 10:47 PM +05:30

--- week2.c
+++ week2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-const int  length = 3;
+const int  N = 3;
 float get_average(int numbers[]);
 
 int main(void)
 {
 
-    int scores[length];
-    for(int i = 0 ; i < length ; i++)
+    int scores [N];
+    for(int i = 0 ; i < N ; i++)
     {
         scores[i]=get_int("Score: ");
 
     }
-    printf("%f \n",get_average(scores));
+    printf("%f \n",get_average( N , scores));
 }
 
-float get_average(int numbers[])
+float get_average(int length ,int numbers[])
 {
 
     int sum = 0;
