410488736a4c38f4979ac73aed208b265cc41d1e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-03 16:44:52 +0000

    Tue, Oct 3, 2023, 10:14 PM +05:30

--- uppercase.c
+++ uppercase.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+
 
 int main(void)
 {
-    string name = get_string("Before: \n");
+    string name = get_string("Before: ");
+    printf("After : ");
     for(int i=0 ; i<strlen(name) ; i++)
     {
-        name[i]
+        if (name[i]>='a' && name[i]<='z' )
+        {
+            printf("%c",name[i]-32);
+        }
+        else
+        {
+            printf("%c", name[i]);
+        }
     }
+
+    printf("\n");
 }
