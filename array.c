3f4bb73d0d1e013bbf3dfaa9d2054b322188f24b
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-04 15:52:37 +0000

    Wed, Oct 4, 2023, 9:22 PM +05:30

---
+++ array.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int array[100];
+    int num=0;
+
+    for(int i=0 ; i<100 ; i++)
+    {
+        array[i]=num;
+        num++;
+        printf("%i ",num);
+
+    }
+
+}
