81f604831567563d776f66e6384ba6c1d731d9db
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-05 09:29:07 +0000

    Thu, Oct 5, 2023, 2:59 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,5 +24,10 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    
+    string word;
+    length = strlen(word);
+    for(i =0 ; i< length ; i++)
+    {
+        if( )
+    }
 }
