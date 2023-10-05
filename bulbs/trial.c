d67026dee5345205eedf636c3eae9553437385bb
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-05 13:46:52 +0000

    Thu, Oct 5, 2023, 7:16 PM +05:30

--- bulbs/trial.c
+++ bulbs/trial.c
@@ -1,9 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h
+#include <string.h>
 
 int main(void)
 {
     string word = get_string(" Message: ");
+    int length = strlen(word);
+    for( int i = 0 ; i < length ; i++)
+    {
+        
+    }
 
 }
