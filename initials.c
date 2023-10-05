f923e096c1c4cce2788f9774b5645724405ce995
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-05 08:34:45 +0000

    Thu, Oct 5, 2023, 2:04 PM +05:30

--- initials.c
+++ initials.c
@@ -5,7 +5,7 @@ int main( int argc , string argv[] )
 {
     for ( int i = 1; i < argc ; i++)
     {
-        printf("%c", argv[i][0]);
+        printf("%c.", argv[i][0]);
     }
     printf("\n");
 }
