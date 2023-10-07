f0ff8d887f8c0da0d75e1fa48258aff340facbef
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-07 14:51:36 +0000

    Sat, Oct 7, 2023, 8:21 PM +05:30

--- bulbs/trial2.c
+++ bulbs/trial2.c
@@ -7,9 +7,8 @@ int main(void)
     int num = 65;
     for(int i = 0 ; i < 8; i ++)
     {
-    int result= pow(2,i);
-    int rem1= 72%(result);
-    rem1
+    rem1 = num%2;
+    
     printf("%i . ", rem1);
     }
 }
