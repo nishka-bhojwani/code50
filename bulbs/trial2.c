262f8d1c3056536852b1535b2cd10d31c12126fd
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-07 14:48:27 +0000

    Sat, Oct 7, 2023, 8:18 PM +05:30

--- bulbs/trial2.c
+++ bulbs/trial2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
     int num = 65;
     for(int i = 0 ; i < 8; i ++)
     {
-    int rem1= 72%();
+    int result= pow(2,i);
+    int rem1= 72%(result);
+    rem1
     printf("%i . ", rem1);
     }
 }
