5a8bbf4d08a360611440f753780cde81a19b8328
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-07 18:29:03 +0000

    Sat, Oct 7, 2023, 11:59 PM +05:30

--- yay.c
+++ yay.c
@@ -5,9 +5,9 @@ int main(void)
 {
     int num = 72;
     int rem[8];
-    for (int i = 0 ; i < 7 ; i++ )
+    for (int i = 0 ; i < 8 ; i++ )
     {
-    rem[8-i]= (int)(num % 2) ;
+    rem[7-i]= (int)(num % 2) ;
     num = num / 2 ;
     printf("%i" , rem[i]);
     }
