0bf063f1563713bcd55abb075e3f12167c2cf1c6
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-07 18:43:11 +0000

    Sun, Oct 8, 2023, 12:13 AM +05:30

--- bulbs/trial.c
+++ bulbs/trial.c
@@ -4,7 +4,8 @@
 
 int main(void)
 {
-    char word[] = get_string(" Message: ");
+    int rem[8];
+    string word = get_string(" Message: ");
     int length = strlen(word);
     for( int i = 0 ; i < length ; i++)
     {
@@ -19,19 +20,4 @@ int main(void)
     }
 
     }
-    int num = 72;
-    int rem[8];
-
-
-    for (int j = 0 ; j < 8 ; i++ )
-    {
-    rem[7-i]= (int)(word[i] % 2) ;
-    word[i] = word[i] / 2 ;
-    }
-    for ( int j = 0 ; j < 8 ; j++)
-    {
-    printf("%i" , rem[j]);
-    }
-
-
 }
