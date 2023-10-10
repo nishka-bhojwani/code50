65e7c16a83043824bd5396cdaa59477c583aa649
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-10 19:33:53 +0000

    Wed, Oct 11, 2023, 1:03 AM +05:30

--- caesar/trial.c
+++ caesar/trial.c
@@ -20,29 +20,16 @@ int main(int argc, string argv[])
 }
 
 bool only_nums(string x)
-{   int cool;
+{
     int length = strlen(x);
     for(int i = 0 ; i < length ; i++ )
     {
-    if( ((int) x[i] < 0 ) || ((int) x[i] > 9 ))
-    {
-        return cool=1;
-    }
-    else
-    {
-        return cool=0;
-    }
-    }
-    if(cool==1)
-    {
-        return true;
-    }
-    if(cool==0)
+    if( ((int) x[i] < '0' ) || ((int) x[i] > '9' ))
     {
         return false;
     }
-
-
+    }
+    return true;
 
 
 }
