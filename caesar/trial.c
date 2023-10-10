77b4fb8d6e14aae5cfafaf9fcc8e94621f2d6197
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-10 19:14:51 +0000

    Wed, Oct 11, 2023, 12:44 AM +05:30

--- caesar/trial.c
+++ caesar/trial.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
-bool only_nums()
+bool only_nums(sring x);
 
 int main(int argc, string argv[])
 {
     if(argc == 2 )
     {
-        return 1;
+        return 0;
     }
     else
     {
         printf("Usage: ./caesar key\n");
+        return 1;
     }
 
 
+}
+
+bool only_nums(string x)
+{
+    if( x == (string) 0 
 }
