5c5f0e9943a0b908263b6cc5051aa0f80e02bdc6
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-13 16:44:54 +0000

    Fri, Oct 13, 2023, 10:14 PM +05:30

--- rotate.c
+++ rotate.c
@@ -1,13 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 char rotate(char letter);
-int key;
+
 
 int main(void)
 {
-    string text = get_string("plaintext: \n");
+    string text = get_string("plaintext: ");
+    int
     printf("ciphertext:");
     for ( int i = 0 ; i < strlen(text) ; i++)
     {
@@ -15,12 +17,12 @@ int main(void)
         {
             if(isupper(text[i]))
             {
-                char final = rotate(text[i]);
+                char final = rotate(text[i], 3);
                 printf("%c", final);
             }
             else if(islower(text[i]))
             {
-                char final2 = rotate(text[i]);
+                char final2 = rotate(text[i] , 3);
                 printf("%c" , final2);
             }
         }
@@ -32,21 +34,21 @@ int main(void)
 
 }
 
-char rotate(char letter)
+char rotate(char letter, int key )
 {
 
     if(isupper(letter))
     {
         letter = (letter/65) ;
-        int c =  ((letter + key ) % 26 )
-        char c = (char) (c1+65)
+        int c =  ((letter + key ) % 26 );
+        c = (char) (c+65);
         return c;
             }
     else if(islower(letter))
     {
         letter = (letter/97) ;
-        int c =  ((letter + key ) % 26 )
-        char c = (char) (c1+97)
+        int c =  ((letter + key ) % 26 );
+        c = (char) (c+97);
         return c;
     }
     return 0;
