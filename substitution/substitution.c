1d8f470b3d6a473ebb6f9af179efbdf1a51ee4ea
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-14 06:31:54 +0000

    Sat, Oct 14, 2023, 12:01 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,13 +7,13 @@
 bool only_26(string x);
 int main(int argc, string argv[])
 {
-    if (argc ==2 && only_26(argv[1]) == true) 
+    if (argc ==2 && only_26(argv[1]) == true)
     {
-        if(strlen(argv[1]) =! 26)
+        if(strlen(argv[1]) != 26)
         printf("Key must contain 26 charecters.");
         return 1;
     }
-    if (argc =! 2 || only_26(argv[1]) == false)
+    if (argc != 2 || only_26(argv[1]) == false)
     {
         printf("Usage: ./substitution key");
     }
@@ -22,8 +22,8 @@ int main(int argc, string argv[])
 
 bool only_26(string x)
 {
-    length = strlen(x);
-    for (i = 0; i < length; i++)
+    int length = strlen(x);
+    for (int i = 0; i < length; i++)
    {
         if (( x[i] >= 'a' && x[i] <= 'z') || ( x[i] >= 'A' && x[i] <= 'Z'))
     {
