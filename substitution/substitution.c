eb311510c3d20a9da0a295ca7e2aaafafa18ba90
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-14 06:23:22 +0000

    Sat, Oct 14, 2023, 11:53 AM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,8 +4,25 @@
 #include <stdlib.h> //for converting string to int
 #include <string.h> //for using strlen function
 
-bool only_nums(string x);
+bool only_26(string x);
 int main(int argc, string argv[])
 {
+    if (argc != 2 || only_26(argv[1]) == false) // condition to check that the user inputs only one key as a CLA
+    {
+        printf("Usage: ./caesar key\n"); // incase user gives more than 1 input tells the user the required input structure
+        return 1;
+    }
+}
 
+bool only_26(string x)
+{
+    length = strlen(x);
+    for (i = 0; i < length; i++)
+   {
+        if (length == 26 && (( x[i] >= 'a' && x[i] <= 'z') || ( x[i] >= 'A' && x[i] <= 'Z')) )
+    {
+        return true;
+    }
+   }
+    return false;
 }
