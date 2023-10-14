f5360327ca4019142d425e226d883679c820b81d
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-14 04:50:38 +0000

    Sat, Oct 14, 2023, 10:20 AM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
+#include <ctype.h> //for using isupper islower and isalpha
 #include <stdio.h>
+#include <stdlib.h> //for converting string to int
+#include <string.h> //for using strlen function
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    
 }
