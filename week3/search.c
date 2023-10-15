7f3c5bcd3df387aacbb53251e1534637c699a82c
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 08:20:03 +0000

    Sun, Oct 15, 2023, 1:50 PM +05:30

--- week3/search.c
+++ week3/search.c
@@ -3,6 +3,18 @@
 
 int main(void)
 {
-    int numbers[] = {20 , 500 , 10 , 5 , 100 , 15};
+    int numbers[] = {20 , 50 , 10 , 5 , 100 , 15};
+
+    int num= get_int("Number: ");
+    for (int i = 0; i < 7 ; i++)
+    {
+        if(num==numbers[i])
+        {
+            printf("Yayyyy found it \n" );
+            return 0;
+        }
+    }
+    printf("Not found :( \n" );
+    return 1;
 
 }
