a5fb4cd34e6c9f63bf9c2797f0fa6a3c557fa654
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 06:16:01 +0000

    Sun, Oct 15, 2023, 11:46 AM +05:30

--- week3/linear_search.c
+++ week3/linear_search.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    int doors[];
-    doors = {10,20,30,40,50,60,70,80,90,100}
+    int doors[10];
+    doors[10] = (10,20,30,40,50,60,70,80,90,100);
     for( int i = 0; i < 10 ; i++)
     {
         if (doors[i]==50)
