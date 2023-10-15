68a38c13eca6b56c91aa0d729ddba0d8a7505ee7
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 06:14:05 +0000

    Sun, Oct 15, 2023, 11:44 AM +05:30

---
+++ week3/linear_search.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int doors[];
+    doors = {10,20,30,40,50,60,70,80,90,100}
+    for( int i = 0; i < 10 ; i++)
+    {
+        if (doors[i]==50)
+        {
+            printf("Yayy I found 50, %i \n" , i);
+            return 1;
+        }
+        return 0 ;
+    }
+
+
+}
