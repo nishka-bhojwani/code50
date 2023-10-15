b9d5f34ced9199da346a18985d6e3712dd541831
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 08:40:13 +0000

    Sun, Oct 15, 2023, 2:10 PM +05:30

---
+++ week3/phonebook.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string names[]= {"Carter" , "David"};
+    string numbers[] = {"+1-655-102-324" , "+1-403-564-555"}
+
+    for( int i= 0 ; )
+}
