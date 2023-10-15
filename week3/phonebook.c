248d864c35f7d35e68797abe1484256e6a5c1edb
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 12:51:45 +0000

    Sun, Oct 15, 2023, 6:21 PM +05:30

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -5,7 +5,17 @@
 int main(void)
 {
     string names[]= {"Carter" , "David"};
-    string numbers[] = {"+1-655-102-324" , "+1-403-564-555"}
+    string numbers[] = {"+1-655-102-324" , "+1-403-564-555"};
+    string name= get_string("Name: ");
 
-    for( int i= 0 ; )
+    for( int i= 0 ; i < 2 ; i++)
+    {
+        if(strcmp(name,names[i])==0)
+        {
+            printf("Found number: %s \n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found \n");
+    return 1;
 }
