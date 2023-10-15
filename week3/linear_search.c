d6fc92cf2d7f7ebf2d70f1bc2de8c5412121c40c
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-15 06:18:02 +0000

    Sun, Oct 15, 2023, 11:48 AM +05:30

--- week3/linear_search.c
+++ week3/linear_search.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    int doors[10];
-    doors[10] = (10,20,30,40,50,60,70,80,90,100);
+
+    int doors[10] = {10,20,30,40,50,60,70,80,90,100};
     for( int i = 0; i < 10 ; i++)
     {
         if (doors[i]==50)
@@ -13,8 +13,8 @@ int main(void)
             printf("Yayy I found 50, %i \n" , i);
             return 1;
         }
-        return 0 ;
     }
+    return 0; 
 
 
 }
