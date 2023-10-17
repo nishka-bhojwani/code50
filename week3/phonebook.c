914019235eefbf8528cf064febbeb34aa625147e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-17 06:33:43 +0000

    Tue, Oct 17, 2023, 12:03 PM +05:30

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -2,17 +2,26 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person people[2];
+
 int main(void)
 {
-    string names[]= {"Carter" , "David"};
-    string numbers[] = {"+1-655-102-324" , "+1-403-564-555"};
+    people[0].name = "Carter";
+    people[1].name = "David";
+    people[0].number= "+1-437-548-1000";
+    people[1].number = "+1-234-897-1600";
     string name= get_string("Name: ");
 
     for( int i= 0 ; i < 2 ; i++)
     {
-        if(strcmp(name,names[i])==0)
+        if(strcmp(name,people[i].name)==0)
         {
-            printf("Found number: %s \n", numbers[i]);
+            printf("Found number: %s \n", people[i].number);
             return 0;
         }
     }
