1e8a31e65296601dad77a1c1a7a7a4b348e71cb6
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-20 02:45:33 +0000

    Fri, Oct 20, 2023, 8:15 AM +05:30

--- practice.c
+++ practice.c
@@ -6,9 +6,9 @@ int main(void)
 {
     for( int i = 1 ; i < 6 ; i++)
     {
-        for(int j = 1 ; j < i+1 ; j++)
+        for(int j = 5 ; j < 1 ; j--)
         {
-            printf(
+            printf(' '
         }
         printf("\n");
     }
