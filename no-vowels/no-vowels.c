b29d15491083a2fcf0bcc2d829905bfd16c283b9
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-24 12:23:09 +0000

    Tue, Oct 24, 2023, 5:53 PM +05:30

---
+++ no-vowels/no-vowels.c
@@ -0,0 +1,40 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+
+char replace(char letter);
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: ./no-vowels word");
+        return 0 ;
+    }
+    replace(argv[1])
+    printf()
+}
+
+string replace(string word)
+{
+    for(int i = 0 ; i < strlen(word); i++)
+    {
+        switch(word[i])
+        {
+        case 'a':
+            return 6;
+        case 'e':
+            return 3;
+        case 'i':
+            return 1;
+        case 'o':
+            return 0;
+    }
+    }
+    return word;
+
+}
