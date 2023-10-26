d65a200b10e7c52f5c59e7dcaf22ae75f90d8597
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-26 13:29:53 +0000

    Thu, Oct 26, 2023, 6:59 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
  for ( int i = 0 ; i < candidate_count ; i++ )
     if(strcmp(name,candidates[i].name)==0)
     {
-        preferences[voter][rank]=candidates[i];
+        preferences[voter][rank]=i; //candidate index assigned to that possition
         return true;
     }
     return false;
@@ -140,7 +140,7 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-
+    for (int i = 0 ; i < )
     return;
 }
 
