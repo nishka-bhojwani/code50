029203b12b2201d8d5a9ae669f88e3a08c2602f3
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-26 13:26:11 +0000

    Thu, Oct 26, 2023, 6:56 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
  for ( int i = 0 ; i < candidate_count ; i++ )
     if(strcmp(name,candidate[i].name)==0)
     {
-        
+        preferences[voter][rank]=candidates[i];
     }
 }
 
