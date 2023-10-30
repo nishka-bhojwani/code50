3292d517fa05aac6eedbae99fa8b53cb49f0ad69
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-10-30 06:45:47 +0000

    Mon, Oct 30, 2023, 12:15 PM +05:30

--- banking.c
+++ banking.c
@@ -24,16 +24,36 @@ int main(void)
 }
 
 
-void create_account(int account_number , char holder_name[] ,float balance)
-{
+void create_account(int account_number , char holder_name[] ,float initial_balance)
 
+{
     if(account_count < MAX_ACCOUNTS)
     {
-        accounts[account_count].holder_name=holder_name[];
+        strcpy(accounts[account_count].holder_name, holder_name);
         accounts[accounts_count].account_number=account_number;
-        accounts[accounts_count].balance=balance;
+        accounts[accounts_count].balance=initial_balance;
         account++;
-        printf(")
+        printf(" Account created successfully! \n");
+    }
+    else
+    {
+        printf("Max account limit exceded can not create new account \n" );
     }
 }
+
+void deposit(int account_number , float deposit)
+{
+    for (int i = 0 ; i < account_number < i++)
+    {
+        if(account_number == accounts[i].account_number)
+        {
+            accounts[i].balance += deposit;
+             printf("Deposited %.2f into account %d. New balance: %.2f\n", amount, account_number, accounts[i].balance);
+            return;
+        }
+    }
+     printf("Account not found. Deposit failed.\n");
+
+}
+
 
