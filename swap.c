535bd45c3f23a6e8e4499d6688997ee72e39a518
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-01 09:44:37 +0000

    Wed, Nov 1, 2023, 3:14 PM +05:30

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int a , int b);  //declaring a function called swap
+
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+    swap()
+}
+
+void swap(int a , int b)
+{
+    int temp;
+    temp = *b;
+    *b = *a;
+    *a = temp;
+}
