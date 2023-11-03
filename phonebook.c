a0476980e73a9e437e44c851a20f500e8e9c6338
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-03 16:14:32 +0000

    Fri, Nov 3, 2023, 9:44 PM +05:30

--- phonebook.c
+++ phonebook.c
@@ -9,6 +9,8 @@ int main(void)
     FILE *file = fopen("phonebook.csv" , "a");
     string name = get_string("Name: ");
     string number = get_string("Phone number: ");
-    fprintf(file , " Name: %s , Phone number: %s" , name , number);
+    fprintf(file , " Name: %s , Phone number: %s " , name , number);
+    fprintf(file ,"\n");
     fclose(file);
+
 }
