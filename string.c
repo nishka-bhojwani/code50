ee4469e86474c0049346d010f733e39f40bd1c10
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-03 06:09:54 +0000

    Fri, Nov 3, 2023, 11:39 AM +05:30

--- string.c
+++ string.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
+#include <string.h>
+
 
 int main(void)
 {
-    char *s;
-    s = "HI";
-    printf("%c" , s[2]);
+    char *s = get_string ("s: \n")
+    char *t = malloc(strlen(s)+1);
+    for (int i = 0, int n = strlen(s)+1  ; i < n ; i++)
+    {
+        s[i]=t[i];
+    }
+    t[0]=toupper(t[0]);
+
+    printf("%s" , s);
+    printf("%s" , t);
+
 }
