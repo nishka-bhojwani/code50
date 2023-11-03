bb02d77e1b978b9989338c1bd2bf9f19e5fa5ffc
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-03 18:34:29 +0000

    Sat, Nov 4, 2023, 12:04 AM +05:30

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -5,9 +5,9 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     // Change all black pixels to a color of your choosing
     for (int i = 0 ; i < height ; i++)
     {
-        for (int j = 0 ; j < i ; j++)
+        for (int j = 0 ; j < width ; j++)
         {
-            if()
+            if(images[i][j]==)
         }
 
     }
