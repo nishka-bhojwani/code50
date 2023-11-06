b573d063306fa13f194a61ee6cc2e976287b04d6
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-06 09:01:33 +0000

    Mon, Nov 6, 2023, 2:31 PM +05:30

--- self.c
+++ self.c
@@ -37,7 +37,7 @@ void make_array(int n, int array[n][n])  //creating an array
     {
         for (int l = 0  ; l < n ; l++)
         {
-            array[m][l]= (1+m+l ) ;
+            array[m][l]= () ;
             printf("%i " , array[m][l]);
         }
         printf("\n");
