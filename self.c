ed69b2c7ba5180e834d62b77b63f6cb126a136fc
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-06 08:29:13 +0000

    Mon, Nov 6, 2023, 1:59 PM +05:30

--- self.c
+++ self.c
@@ -1,14 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
+int size = 8 ;
+int array[n][n] make_array(int n); //creating an array of a given size
+
 int main(void)
 {
-    int images[8][8];
+
+    images[size][size] =make_array(int size);
     for (int i = 0 ; i < 8 ; i++)
     {
         for(int j = 0 ; j<8 ; j++)
         {
-            
+            images[i][j]=images[i][j-1];
+            printf("%i" , images[i][j]);
+        }
+
+    }
+    printf("\n")
+}
+
+int array[n][n] make_array(int n);  //creating an array
+{
+    for ( int m = 0 ; m < n  ; m++)
+    {
+        for (int l = 0  ; l < m ; l++)
+        {
+            array[m][l]= (m+1)*3 ;
         }
     }
+    return int array[m][l];
 }
