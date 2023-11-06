4374fd16cdaa1456520be14142c78e44dcaf3aae
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-06 08:14:16 +0000

    Mon, Nov 6, 2023, 1:44 PM +05:30

--- self.c
+++ self.c
@@ -7,10 +7,8 @@ int main(void)
     for (int i = 0 ; i < 8 ; i++)
     {
         for(int j = 0 ; j<8 ; j++)
-
-
-    {
-        images[i][0]= i*3;
-    }
+        {
+            
+        }
     }
 }
