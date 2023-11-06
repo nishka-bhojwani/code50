8b8b0263e04a6c4a0853346ec364014003bd07c0
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-06 10:12:58 +0000

    Mon, Nov 6, 2023, 3:42 PM +05:30

--- self2.c
+++ self2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+//defining a structure that has 3 integers stored
+typedef struct
+{
+    int black = 12;
+    int green = 10;
+    int blue = 15;
+}
+color;
+
+color image[][];
+
 
 int main(void)
 {
-    
+
 }
