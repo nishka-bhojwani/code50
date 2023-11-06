c7cd0bca7a240b7424802577e072427e2d3cc82b
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-06 09:10:04 +0000

    Mon, Nov 6, 2023, 2:40 PM +05:30

--- self.c
+++ self.c
@@ -11,22 +11,14 @@ int main(void)
     make_array(size, images);
     printf("\n");
     printf("\n");
-    for (int i = 0 ; i < 8 ; i++)
-    {
-        for(int j = 0 ; j<8 ; j++)
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = size - 1; j >= 0; j--)
         {
-            if(j==0 )
-            {
-                images[i][j]=images[i][j];
-                printf("%i " , images[i][j]);
-            }
-            else
-            {
-            images[i][j]=images[i][size-j - 1];
-            printf("%i " , images[i][j]);
-            }
+            printf("%i ", images[i][j]);
         }
-    printf("\n");
+        printf("\n");
     }
 
 }
