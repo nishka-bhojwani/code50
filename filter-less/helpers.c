b9c258430c1af55e6220f8ea836c1946669154c9
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-07 11:22:58 +0000

    Tue, Nov 7, 2023, 4:52 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,7 +19,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0 ; i < height ; i++)
+    {
+        for( int j = 0 ; j < width ; j++)
+        {
+            n = 
+        }
+    }
+
     return;
 }
 
