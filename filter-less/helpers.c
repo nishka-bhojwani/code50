a4711203c67a0baa742a10c0ff7d2680a890d6a5
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-07 12:11:28 +0000

    Tue, Nov 7, 2023, 5:41 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -57,9 +57,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0 ; j <= (width - 1) / 2 ; j--)
+        for (int j = 0 ; j < width  / 2 ; j++)
         {
-             
+             RGBTRIPLE temp = image[i][j];
+             image[i][j] = image[i][width - j - 1];
+             image[i][width - j - 1] = temp;
         }
     }
     return;
