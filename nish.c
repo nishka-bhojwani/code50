48a5b212301b99a699113f85d29c39f1e6502cc4
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-08 04:51:13 +0000

    Wed, Nov 8, 2023, 10:21 AM +05:30

---
+++ nish.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int matrix[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] = i * 3 + j + 1;
+            printf("%i",matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}
+
