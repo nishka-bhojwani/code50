039ddaa4310702d239337dac225b188e84c12746
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-09 21:57:38 +0000

    Fri, Nov 10, 2023, 3:27 AM +05:30

---
+++ section_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+int main(int argc , string argv[])
+{
+    if(argc!=0)
+    {
+        printf("Usage: ./section_4 input_file \n");
+        return 1;
+    }
+    
+}
