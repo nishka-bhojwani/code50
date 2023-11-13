76b02a82be6e6065550745b7a2e279b2af02b1e1
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-13 09:21:32 +0000

    Mon, Nov 13, 2023, 2:51 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
 
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    
 
     // Close files
     fclose(input);
