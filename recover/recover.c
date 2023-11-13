a6953e7812a35d82cba24a823a89a96e083ba793
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-13 10:24:58 +0000

    Mon, Nov 13, 2023, 3:54 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover card.raw \n");
+    }
+
+    FILE* new_file = fopen(card.raw , 'r');
+    if(card.raw == NULL)
+    {
+        printf("invalid file \n");
+    }
+
 }
