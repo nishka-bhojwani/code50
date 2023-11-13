047543bdf1a880fa86053195f86763fecd2df2e2
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-13 12:50:16 +0000

    Mon, Nov 13, 2023, 6:20 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -28,10 +28,12 @@ int main(int argc, char *argv[])
     char *file_name = malloc(8*sizeof(char));
 
     //iterating through 512 bytes of memory in the input file
-    fread(buffer , sizeof(char),512,input_file );
+    while(fread(buffer , sizeof(char),512,input_file ))
+{
     if( buffer[0]== 0xff && buffer[1]== 0xd8 && buffer[2]==0xff &&(buffer[3] & 0xf0 == 0xe0 ))
     {
 
     }
+
 
 }
