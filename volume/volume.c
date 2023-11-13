9ec0cd7a6ee82bcf8cbcbb3e6077e92c8f74dc4d
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-13 10:11:55 +0000

    Mon, Nov 13, 2023, 3:41 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -35,17 +35,17 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t HEADER[HEADER_SIZE]; //breaking 44 bits into 8 bit of blocks and storing it as an array(space in memory) so that it can be used later
-    fread(HEADER, HEADER_SIZE , 1 , input);
-    fwrite(HEADER, HEADER_SIZE , 1 , output);
-    
+    fread(HEADER, HEADER_SIZE , 1 , input); //reads 44 bits of memory from input file that we have stored as HEADER
+    fwrite(HEADER, HEADER_SIZE , 1 , output);//writes that into the output file
+
 
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t buffer;
-    while(fread(&buffer, sizeof(int16_t) , 1 , input))
+    int16_t buffer;  //creating a block of 2 bytes of memory in the input file after the header file so that 2 bytes can be copied at a time
+    while(fread(&buffer, sizeof(int16_t) , 1 , input)) //using the while loop to stop reading data after the input file ends
     {
         buffer = buffer*factor;
-        fwrite(&buffer , sizeof(int16_t) , 1 , output);
+        fwrite(&buffer , sizeof(int16_t) , 1 , output); //writing that data in output file 
     }
 
     // Close files
