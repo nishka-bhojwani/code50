0825493efa517b6aedd0ba460589047b843398fc
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-16 10:34:58 +0000

    Thu, Nov 16, 2023, 4:04 PM +05:30

--- linked_list.c
+++ linked_list.c
@@ -13,16 +13,16 @@ int main(int argc ,char *argv[]) //takes command line arguments
     node *list = NULL; //declaring a variable called list that acts as an empty block to declare the starting of a list
     for (int i = 1 , i < argc ; i++) //iterating over the command line arguments
     {
-        int number = atoi(argv[i]); //stoi converts string to an integer 
+        int num = atoi(argv[i]); //stoi converts string to an integer
     node *n = malloc(sizeof(node));
     if(n==NULL)
     {
         return 1;
     }
-    n->number = number;
-    n->next = NULL;
+    n->number = num; // considering the case of first command line argument , the 1st node number value becomes first CLA
+    n->next = NULL; // clearing up the address space for next variable in that particular node
 
-    n->next = list;
+    n->next = list; //new node points to the list (prepend)
     list = n ;
     }
     node *ptr = list;
