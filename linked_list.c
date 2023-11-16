5255fd5a58576c51beb6b6dda6c760dde9cf6ef5
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-16 10:09:58 +0000

    Thu, Nov 16, 2023, 3:39 PM +05:30

--- linked_list.c
+++ linked_list.c
@@ -13,8 +13,20 @@ int main(int argc ,char *argv[])
     node *list = NULL;
     for (int i = 1 , i < argc ; i++)
     {
-        int number = argv[i]
+        int number = atoi(argv[i]);
+
     }
+
+    node *n = malloc(sizeof(node));
+    if(n==NULL)
+    {
+        return 1;
+    }
+    n->number = number;
+    n->next = NULL;
+
+    n->next = list;
+    list = n ; 
 
 
 }
