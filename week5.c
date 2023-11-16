b2777343d4c8bfe542d610a77840adc37c6ca545
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-16 13:33:54 +0000

    Thu, Nov 16, 2023, 7:03 PM +05:30

--- week5.c
+++ week5.c
@@ -12,10 +12,21 @@ int main(void)
     list[0] = 1;
     list[1] = 2 ;
     list[2] = 3;
+    //creating a temprary list to copy the data from list and then make changes to it
 
-    for(int i = 0 ; i < 3 ; i++)
+    int *tmp = malloc(sizeof(int));
+    if(tmp==NULL)
     {
-        printf("%i \n" , list[i]);
+        free(list);
+        return 1;
     }
+
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3]=4;
+    free(list);
+    list = tmp;
 
 }
