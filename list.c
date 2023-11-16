c65b139f4cca6ac62fad05c1dc4d3e81faba6e90
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-16 18:22:16 +0000

    Thu, Nov 16, 2023, 11:52 PM +05:30

--- list.c
+++ list.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- 
+ //defining a node
+ typedef struct node
+  {
+    int number ;
+    node *next; //defining the address of the next node
+ }
+ node;
+
+ //defining a variable called list which is an empty node
+
+
