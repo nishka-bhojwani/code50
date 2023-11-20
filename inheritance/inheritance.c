64b3cf3d495531909b1ef67615c51a838d8e8ce0
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-20 09:07:44 +0000

    Mon, Nov 20, 2023, 2:37 PM +05:30

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -45,8 +45,6 @@ person *create_family(int generations)
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        p->parents[0]=NULL;
-        p->parents[1]=NULL;
         p->parents[0] = create_family(generations - 1);
         p->parents[1] = create_family(generations - 1);
 
