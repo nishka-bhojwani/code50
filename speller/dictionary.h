c1c7fb6b8cd5e53b844092241d069adcca373443
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2023-11-21 05:39:17 +0000

    Tue, Nov 21, 2023, 11:09 AM +05:30

--- speller/dictionary.h
+++ speller/dictionary.h
@@ -10,9 +10,9 @@
 #define LENGTH 45
 
 // Prototypes
-bool check(const char *word); //char * represents that the word will be a string
-unsigned int hash(const char *word); //
-bool load(const char *dictionary);
+bool check(const char *word); //char * represents that the word will be a string , o/p is a boolean
+unsigned int hash(const char *word); //input is a constant string , o/p is an unsigned integer(non negative)
+bool load(const char *dictionary); //string dictionary , output is a boolean 
 unsigned int size(void);
 bool unload(void);
 
