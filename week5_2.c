10dcb3b41579c01706336ea6c6ef29af69dfd1ee
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-01-03 17:52:07 +0000

    Wed, Jan 3, 2024, 11:22 PM +05:30

--- week5_2.c
+++ week5_2.c
@@ -22,8 +22,8 @@ int main(void)
 int main(void)
 {
     int *list = malloc(3*sizeof(int));
-    list[0] = 1;
-    list[1] = 2 ;
+    list[0] = 1;  //can also be written as *list
+    list[1] = 2 ; //*(list+1)
     list [2] = 3;
 
     for (int i = 0 ; i < 3  ; i++)
