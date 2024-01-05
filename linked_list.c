b37453bdeba17960da67e56584de1a63f99ead14
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-01-05 11:28:12 +0000

    Fri, Jan 5, 2024, 4:58 PM +05:30

--- linked_list.c
+++ linked_list.c
@@ -3,42 +3,18 @@
 
 typedef struct node
 {
-    int number ;
-    struct node *next;
+    int number;
+    struct node *next ;
 }
-node;
+node
 
-int main(int argc ,char *argv[]) //takes command
-
- line arguments
+int main (int argc ; char* argv[])
 {
-    node *list = NULL; //declaring a variable called list that acts as an empty block to declare the starting of a list
-    for (int i = 1 ; i < argc ; i++) //iterating over the command line arguments
-    {
-        int num = atoi(argv[i]); //stoi converts string to an integer
-        node *n = malloc(sizeof(node));
-    if(n==NULL)
-    {
-        return 1;
-    }
-    n->number = num; // considering the case of first command line argument , the 1st node number value becomes first CLA
-    n->next = NULL; // clearing up the address space for next variable in that particular node
 
-    n->next = list; //new node points to the list (prepend)
-    list = n ; // appending the list
-    }
-    node *ptr = list; //points to first node in the list
-    while(ptr !=NULL)
-    {
-        printf("%i \n", ptr->number);
-        ptr = ptr->next; //ptr->next go to the next number and therefore the pointer points at the second node
-    }
-    ptr = list; //gives a temporary pointer that points at 3
-    while(ptr != NULL)
-    {
-        node *next_value = ptr->next ;
-        free(ptr);
-        ptr = next_value ;
-    }
+    node n = malloc(sizeof(node));
+
+    node->number = 1;
+    node ->
+
 
 }
--- week5.c
+++ week5.c
@@ -14,7 +14,7 @@ int main(void)
     list[2] = 3;
     //creating a temprary list to copy the data from list and then make changes to it
 
-    int *tmp = realloc(list, 4*sizeof(int)); //checking the list values to make sure we're not playing with unassigned memory 
+    int *tmp = realloc(list, 4*sizeof(int)); //checking the list values to make sure we're not playing with unassigned memory
     if(tmp==NULL)
     {
         free(list);
@@ -31,4 +31,45 @@ int main(void)
 
     free(list);
     return 0;
+}
+
+
+typedef struct node
+{
+    int number ;
+    struct node *next;
+}
+node;
+
+int main(int argc ,char *argv[]) //takes commandline arguments
+{
+    node *list = NULL; //declaring a variable called list that acts as an empty block to declare the starting of a list
+    for (int i = 1 ; i < argc ; i++) //iterating over the command line arguments
+    {
+        int num = atoi(argv[i]); //stoi converts string to an integer
+        node *n = malloc(sizeof(node));
+    if(n==NULL)
+    {
+        return 1;
+    }
+    n->number = num; // considering the case of first command line argument , the 1st node number value becomes first CLA
+    n->next = NULL; // clearing up the address space for next variable in that particular node
+
+    n->next = list; //new node points to the list (prepend)
+    list = n ; // appending the list
+    }
+    node *ptr = list; //points to first node in the list
+    while(ptr !=NULL)
+    {
+        printf("%i \n", ptr->number);
+        ptr = ptr->next; //ptr->next go to the next number and therefore the pointer points at the second node
+    }
+    ptr = list; //gives a temporary pointer that points at 3
+    while(ptr != NULL)
+    {
+        node *next_value = ptr->next ;
+        free(ptr);
+        ptr = next_value ;
+    }
+
 }
