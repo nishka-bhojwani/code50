dd81cffd634072e77adb01610d097ef1b48177a2
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-01-27 08:19:00 +0000

    Sat, Jan 27, 2024, 1:49 PM +05:30

---
+++ jan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+}
+node
+
+#define LIST_SIZE 2;
+
+int main(char argc, string argv[])
+{
+
+}
