665abdc0661d583145c3a0ec919d9e7e74e42a10
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-27 19:37:36 +0000

    Tue, May 28, 2024, 1:07 AM +05:30

--- new.py
+++ new.py
@@ -1,5 +1,7 @@
 #creating a variable that stores a value
-name = input("whats your name?") # input function takes an argument from command line and copies that from right to left
-# input is stored in that variable i.e. name 
-print("hello " ,name ) #print function used by new.py , argument is what we input in the parenthesis , side effect - display of hello world , bugs - mistakes
+name = input("whats your name? ") # input function takes an argument from command line and copies that from right to left
+# input is stored in that variable i.e. name
+print("hello",name , sep = "bruh" ) #print function used by new.py , argument is what we input in the parenthesis , side effect - display of hello world , bugs - mistakes
 
+print("hello," , end = "" )
+print(name)
