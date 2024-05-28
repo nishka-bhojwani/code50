806088f841dca744f91fc34471dc85edcbd19ccb
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-28 12:25:33 +0000

    Tue, May 28, 2024, 5:55 PM +05:30

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 #creating a variable that stores a value
 name = input("whats your name? ") # input function takes an argument from command line and copies that from right to left
 # input is stored in that variable i.e. name
-print("hello",name , sep = "bruh" ) #print function used by new.py , argument is what we input in the parenthesis , side effect - display of hello world , bugs - mistakes
+print("hello",name , sep = "" ) #print function used by new.py , argument is what we input in the parenthesis , side effect - display of hello world , bugs - mistakes
 
 print("hello," , end = "" )
 print(name)
@@ -9,5 +9,6 @@ print ("hello , \"friend\"")# \ represents escape charecter
 
 
 #strip - to remove white spaces
-#title for making the first letter of all words capital 
+#title for making the first letter of all words capital
 name = name.strip()
+print(name)
