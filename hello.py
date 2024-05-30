13fc4f511a78293c1d080d4ff9f1f31382e039ef
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-30 12:34:31 +0000

    Thu, May 30, 2024, 6:04 PM +05:30

--- hello.py
+++ hello.py
@@ -4,6 +4,13 @@ def hello(to="world"): #to is the parameter , default value is world(if nothing 
     print(f"hello, {to}")
 
 
-name = input("Whats your name? ")
-hello(name) #name is essentially copied to the variable to
-hello()
+def main():
+    name = input("Whats your name? ")
+    hello(name) #name is essentially copied to the variable "to"
+    hello()
+
+#defining a function called hello which prints hello when called
+def hello(to="world"): #to is the parameter , default value is world(if nothing is inputed)
+    print(f"hello, {to}")
+
+main() #calling main function 
