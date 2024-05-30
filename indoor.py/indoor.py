dbeadc80250a8f7cf5ace0ac29c927694b211b37
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-30 13:13:54 +0000

    Thu, May 30, 2024, 6:43 PM +05:30

---
+++ indoor.py/indoor.py
@@ -0,0 +1,11 @@
+#to create a programme that converts the input to all lowercase
+
+def main():
+    message = str(input("Input: "))
+    print(f"{lowercase(message)}")
+
+#defining a function that converts the given input to lowercase
+def lowercase(convert):
+    return convert.lowercase()
+
+
