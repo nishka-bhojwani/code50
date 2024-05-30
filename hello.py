872f73187abf39f04acd27e0c5a72fde1c3db9f5
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-30 12:31:02 +0000

    Thu, May 30, 2024, 6:01 PM +05:30

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 
 #defining a function called hello which prints hello when called
-def hello(to):
+def hello(to="world"): #to is the parameter , default value is world(if nothing is inputed)
     print(f"hello, {to}")
 
 
 name = input("Whats your name? ")
-hello(name)
-
+hello(name) #name is essentially copied to the variable to
+hello()
