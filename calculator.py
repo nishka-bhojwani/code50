1f45032c48389f0c911fd75a84fddfcd9eb9a43a
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-30 12:23:51 +0000

    Thu, May 30, 2024, 5:53 PM +05:30

--- calculator.py
+++ calculator.py
@@ -2,8 +2,9 @@ x = float(input("x: ")) #input gets called first # the o/p of that becomes the i
 y = float(input("y: "))
 
 z = int(x + y) #takes only the integer part
-n  = round(x+y) #rounds the value (5 gets rounded down ) #syntax round(number[,ndigit])
+n  = round(x+y) #rounds the value (5 gets rounded down ) #syntax round(number,digit)
+d= round(x/y,2)
 
 print(x+y)
-print(f"{z:,}") # commas to group 3 zeroes 
+print(f"{z:,}") # commas to group 3 zeroes
 print(n)
