8caf851f97ede5e45a4c16fb51c4bf3f1ded544f
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-31 18:53:23 +0000

    Sat, Jun 1, 2024, 12:23 AM +05:30

---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+thought = str(input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")).lower()
+
+if thought == "42" or thought == "forty-two" or thought == "forty two":
+    print("Yes")
+else:
+    print("No")
