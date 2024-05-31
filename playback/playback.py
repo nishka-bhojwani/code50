4256680028c0d99cb2b1b3ee05de521983b219c5
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-31 05:26:41 +0000

    Fri, May 31, 2024, 10:56 AM +05:30

--- playback/playback.py
+++ playback/playback.py
@@ -1,5 +1,7 @@
 # to write a programme that replaces space with ...
 message  = str(input("message: "))
+# creating a new variable that replaces the whitespaces with ...
 new = message.replace(" ","...")
+#printing the new function 
 print(new)
 
