fc5c7398e39a5238b5384b14ba1be8d77560037b
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-31 05:35:40 +0000

    Fri, May 31, 2024, 11:05 AM +05:30

--- faces/faces.py
+++ faces/faces.py
@@ -1,4 +1,13 @@
 # to write a functin that converts :) to 🙂  and :( to🙁
 
-def main():
-    
+def main(message):
+    message = str(input("Write your message: " ))
+    print(convert(message))
+
+
+
+def convert(str(input)):
+    new = input.replace(":)","🙂")
+    new = input.replace(":(","🙁")
+    return new
+
