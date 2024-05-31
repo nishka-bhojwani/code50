2b07c154fa2c666478ba4984cfff8513ae219aaf
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-05-31 06:04:46 +0000

    Fri, May 31, 2024, 11:34 AM +05:30

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,7 +1,7 @@
 # to get mass as input and print energy based on formula E=mc^2
 
 c = 300000000 #defining approximate speed of light
-mass = int(input("m: ")) # taking mass as an input (int)
+mass = int(input("m:")) # taking mass as an input (int)
 energy = mass*(c**2) #calculating energy
 
 print(f"E:{energy}")
