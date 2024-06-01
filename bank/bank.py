f476c36a8add59b09ee5e9160c5e01c8ca51f7e7
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-01 05:27:05 +0000

    Sat, Jun 1, 2024, 10:57 AM +05:30

---
+++ bank/bank.py
@@ -0,0 +1,6 @@
+# aim is to code a programme that shows how much money will be deducted in case of different greetings from back manager
+
+def with_hello(message):
+    if message[0]=="hello":
+        
+
