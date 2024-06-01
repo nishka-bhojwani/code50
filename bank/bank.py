137215e308a7afbdcbb291bef6b6d95ca9ef56f3
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-01 05:34:20 +0000

    Sat, Jun 1, 2024, 11:04 AM +05:30

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,18 @@
 # aim is to code a programme that shows how much money will be deducted in case of different greetings from back manager
 
+
+def main():
+    message = str(input("greeting:"))
+    with_hello(message)
+    print(message[0])
+
 def with_hello(message):
-    if message[0]=="hello":
-        
+    if message[0:] == "hello":
+        print("$0")
+    elif message[0][0] == "h":
+        print("$20")
+    else:
+        print("$100")
+
+main()
 
