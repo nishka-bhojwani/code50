6303baf16ff7ba14ec64a3c9be872e546238d474
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-01 10:37:30 +0000

    Sat, Jun 1, 2024, 4:07 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,13 +1,31 @@
+"""
+general suffixes and output for reference
+.gif   - image/gif
+.jpg   - image/jpeg
+.jpeg  - image/jpeg
+.png   - image/png
+.pdf   - application/pdf
+.txt   -  text/plain
+.zip   - application/zip
+"""
+
 file = str(input("file : " ))# takes user input
 new = file.split(".")#splitting the original string into two strings before and after the .
 extension = new[1] # gets the second word after splitting and stores it in extension
+#taking into account all specified conditions printing o/p
 if extension == "gif" or "jpg" or "jpeg" or "png":
     if extension == "jpeg" or "jpg":
         print("image/jpeg")
     else:
         print(f"image/{extension}")
-elif extension == "pdf":
-    print
+elif extension == "pdf" or "zip":
+    print(f"application/{extension}")
+elif extension == "txt":
+    print(f"text/plain")
+else:
+    print("application/octet-stream")
+
+
 
 
 
