0395e3e587dd8870c462f25b2f293fb0e8b69899
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-01 10:02:40 +0000

    Sat, Jun 1, 2024, 3:32 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,6 +1,6 @@
-file = str(input("file : " ))
-new = file.split(".")
-extension = new[1]
+file = str(input("file : " ))# takes user input 
+new = file.split(".")#splitting the original string into two strings before and after the .
+extension = new[1] # gets the second word after splitting and stores it in extension
 print(f"image/{extension}")
 
 
