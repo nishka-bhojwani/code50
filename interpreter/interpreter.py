50401adbe92d53e13eadeac5de51acca2a5a74fd
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-02 12:13:26 +0000

    Sun, Jun 2, 2024, 5:43 PM +05:30

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,20 @@
 #taking input from user
-Expression = str(input("Expression:"))
-new = Expression()
+Expression = str(input("Expression: ")).strip()
+new = Expression.split(" ")
+x = float(new[0])
+y = str(new[1])
+z = float(new[2])
+
+
+if y == "+":
+    print(x+z)
+elif y == "-":
+    print(x-z)
+elif y=="*":
+    print(x*z)
+elif y == "/":
+    if z== 0:
+        print("invalid input")
+    else:
+        print(x/z)
+
