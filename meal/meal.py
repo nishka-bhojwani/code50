705e3e15f733171590ecefd724f935ca069cd2c5
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-03 05:18:37 +0000

    Mon, Jun 3, 2024, 10:48 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,13 +1,17 @@
 
 
 def main():
-    convert()
+    if convert()
 
 
 
 def convert(time):
     time = str(input("What time is it? ")).strip().split(":")
     hours = float(time[0])
+    minutes = float(time[1])
+    return (hours+(minutes/60))
+
+
 
 
 
