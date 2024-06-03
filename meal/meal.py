dc8fe3aff11686dd4cbcbbd7003904fc0d9a4445
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-03 05:23:15 +0000

    Mon, Jun 3, 2024, 10:53 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,18 +1,23 @@
 
 
 def main():
-    if convert()
+    time = str(input("What time is it? ")).strip()
+    if 7.0 < convert(time) < 8.0:
+        print("breakfast time")
+    elif 12.0 < convert(time) < 13.0:
+        print("lunch time")
+    elif 18.0 < convert(time) < 19.0:
+        print("dinner time")
+    else:
+        exit
 
 
 
 def convert(time):
-    time = str(input("What time is it? ")).strip().split(":")
+    time = time.split(":")
     hours = float(time[0])
     minutes = float(time[1])
     return (hours+(minutes/60))
-
-
-
 
 
 
