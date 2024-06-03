6214ee09aa85e40f8a19603f5987cb1d895064bc
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-03 05:26:52 +0000

    Mon, Jun 3, 2024, 10:56 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,7 @@
 
 
 def main():
-    time = str(input("What time is it? ")).strip()
+    time = str(input("What time is it? ")).strip() # taking user input
     if 7.0 <= convert(time) <= 8.0:
         print("breakfast time")
     elif 12.0 <= convert(time) <= 13.0:
@@ -15,9 +15,9 @@ def main():
 
 def convert(time):
     time = time.split(":")
-    hours = float(time[0])
-    minutes = float(time[1])
-    return (hours+(minutes/60))
+    hours = float(time[0]) #taking the first string after splitting and storing it in hours
+    minutes = float(time[1]) #taking the second string after splitting and storing it in minutes
+    return (hours+(minutes/60)) #returning a float number 
 
 
 
