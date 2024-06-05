292ca60c8213947cad9ab2800a2dc37b302a5701
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-05 14:49:28 +0000

    Wed, Jun 5, 2024, 8:19 PM +05:30

---
+++ mario.py
@@ -0,0 +1,12 @@
+def main():
+    print_rows(3)
+
+def print_column(height):
+    for i in range(height):
+        print("#")
+
+def print_rows(width):
+    for i in range(width):
+        print("###")
+
+main()
