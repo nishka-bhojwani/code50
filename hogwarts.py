74641d5270ec87126e241182e213c865fa205043
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-05 13:41:26 +0000

    Wed, Jun 5, 2024, 7:11 PM +05:30

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,20 @@
 students = {
   "Hermione" : "Gryffindor",
-  "Hermione" : "Gryffindor",
-  "Hermione" : "Gryffindor",
-  "Hermione" : "Gryffindor"
-
-
-
+  "Harry" : "Gryffindor",
+  "Ron " : "Gryffindor",
+  "Draco" : "Slytherin"
   }
+
+for student in students:
+    print(student , students[student],sep=",")
+
+
+
+new = [
+    {"name" :"Hermione" , "house": "Gryffindor" , "petrona":"Otter" },
+    {"name" :"Harry" , "house": "Gryffindor" , "petrona":"Stag" },
+     {"name" :"Ron" , "house": "Gryffindor" , "petrona":"deer" },
+      {"name" :"Draco" , "house": "Slytherin" , "petrona":None}
+]
+
+print()
