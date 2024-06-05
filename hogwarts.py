cb8c164a62938575f720207e4b27665223c17659
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-05 13:45:08 +0000

    Wed, Jun 5, 2024, 7:15 PM +05:30

--- hogwarts.py
+++ hogwarts.py
@@ -16,5 +16,5 @@ new = [
      {"name" :"Ron" , "house": "Gryffindor" , "petrona":"deer" },
       {"name" :"Draco" , "house": "Slytherin" , "petrona":None}
 ]
-for student in students:
+for student in new:
     print(new["name"],new["house"],new["petrona"],sep=" ")
