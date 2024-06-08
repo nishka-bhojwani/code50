486d3edfdac9c5438ff659241155e3622f976a6e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-08 08:51:49 +0000

    Sat, Jun 8, 2024, 2:21 PM +05:30

--- camel/camel.py
+++ camel/camel.py
@@ -1,11 +1,17 @@
 # taking input string from user
 
-camel_name = str(input("camelCase:"))
+camel_name = str(input("camelCase: "))
 length = len(camel_name)
 
+print("snake_case: " , end = "")
 for i in range(length):
     if camel_name[i].isupper():
-        print(f"{camel_name[0:i]}_{camel_name[i:length].lower()}")
-        
+        new = camel_name[i].lower()
+        print(f"_{new}",end="")
+    else:
+        print(camel_name[i],end="")
+
+
+print()
 
 
