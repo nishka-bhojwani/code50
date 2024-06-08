6809ddaf4f4840424b8f65188d96bba38eb07ccb
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-08 08:54:47 +0000

    Sat, Jun 8, 2024, 2:24 PM +05:30

--- camel/camel.py
+++ camel/camel.py
@@ -4,10 +4,10 @@ camel_name = str(input("camelCase: "))
 length = len(camel_name)
 
 print("snake_case: " , end = "")
-for i in range(length):
-    if camel_name[i].isupper():
-        new = camel_name[i].lower()
-        print(f"_{new}",end="")
+for i in range(length): #iterating over each charecter in the string
+    if camel_name[i].isupper(): #finding and replacing uppercase charecters with _lowercase
+        new = camel_name[i].lower() #converting and storing lowercase charecter in new
+        print(f"_{new}",end="") # preventing from moving to next line  by changing end 
     else:
         print(camel_name[i],end="")
 
