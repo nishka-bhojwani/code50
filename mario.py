3b12828028e456f85cfbfa476377bda547ee22b1
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-08 05:24:13 +0000

    Sat, Jun 8, 2024, 10:54 AM +05:30

--- mario.py
+++ mario.py
@@ -4,7 +4,7 @@ def main():
 def print_square(size):
         for i in range(size): #for each row in square
               for j in range(size): #for each brick in row
-                    print("#",end="")
+                    print("#",end="") # on the same line
 
-              print()  #new line after the 
+              print()  #new line after the first row of bricks is finished 
 main()
