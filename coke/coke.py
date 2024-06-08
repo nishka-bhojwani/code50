48f5f2a198838b36f4e744b9b480bfbbc5a281fd
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-08 09:58:45 +0000

    Sat, Jun 8, 2024, 3:28 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -5,7 +5,17 @@ if amount==50:
     coin1 = int(input("Insert coin: " ))
     while coin1 not in [25,5,10]:
         coin1 = int(input("Insert coin: " ))
-    
+    amount_due = int(input(f"Amount due: {amount}-{coin1}"))
+    if amount_due =! 0:
+        coin2 = int(input("Insert coin: "))
+    while coin2 not in [25,5,10]:
+        coin2 = int(input("Insert coin: " ))
+    amount_due = int(input(f"Amount due: {amount}-{coin2}"))
+    if amount_due != 0 :
+        print
+
+
+
 
 
 
