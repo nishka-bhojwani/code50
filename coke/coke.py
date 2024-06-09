ae390c2800a61e21e6a164790aac9e9d632d1f56
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-09 07:35:34 +0000

    Sun, Jun 9, 2024, 1:05 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -4,11 +4,13 @@ print("Amount due: 50")
 amount=50
 coin = int(input("Insert coin: "))#taking first coin input
 while coin not in [25,5,10]: # keep prompting till input is in 25,5,10
+    print("Amount Due: 50")
     coin = int(input("Insert coin: "))
 amount_due = amount-coin #calculating due amount and printing it
 print(f"Amount Due: {amount_due}")
 while amount_due > 0 : # checking if due amount is greater than zero and then asking for more coin inputs till it becomes zero
     coin1 = int(input("Insert coin: "))
+    print(f"Amount Due: {amount_due}")
     while coin1 not in [25,5,10]:
         coin1 = int(input("Insert coin: "))
     amount_due = amount_due - coin1
