9a20fd41c4b6145039327ecd57cb488df52613cd
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-09 06:57:46 +0000

    Sun, Jun 9, 2024, 12:27 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,13 +1,19 @@
 #taking user input (only integers 25, 5 and 10 are accepted )
 
 amount = int(input("Amount due: "))
+counter = 0
 if amount==50:
     coin = int(input("Insert coin: "))
     while coin not in [25,15,10]:
-        coin = int(input("Insert coin: "))
+        coin1 = int(input("Insert coin: "))
+    counter = coin
     amount_due = amount-coin
     print(f"Amount due: {amount_due}")
     while amount_due > 0:
-        coin = int(input("Insert coin: "))
-        amount_due = amount - coin
-    if
+        coin1 = int(input("Insert coin: "))
+        amount_due = amount - coin1
+        counter = coin + coin1
+    change = int(amount - counter)
+    print(f"Change due: {change}")
+else:
+    print("Invalid input")
