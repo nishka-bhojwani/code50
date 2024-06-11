b572a65940eeb0352223739d99417e948b5a336a
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-11 13:39:15 +0000

    Tue, Jun 11, 2024, 7:09 PM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -7,7 +7,6 @@ def main():
 
 
 def is_valid(s):
-
     if s.isalnum() and 2<=len(s)<=6:
         if len(s) ==2:
             if s[0].isdigit() or s[1].isdigit()  :
@@ -33,7 +32,6 @@ def is_valid(s):
             else:
                 return False
         elif len(s) == 5:
-
              if s.isalpha():
                 return True
              if s[1].isdigit() or s[0].isdigit():
@@ -43,7 +41,22 @@ def is_valid(s):
              if s[2].isdigit() == False and s[3].isdigit()and s[4].isdigit:
                 return True
              if s[2].isdigit() == False and s[3].isdigit()==False and s[4].isdigit:
-                 return True 
+                 return True
+             else:
+                return False
+        elif len(s) == 6:
+             if s.isalpha():
+                return True
+             if s[1].isdigit()==False and s[0].isdigit()==False:
+                return True
+             if s[2].isdigit() and s[3].isdigit() and s[4].isdigit() and s[5].isdigit():
+                return True
+             if s[2].isdigit() == False and s[3].isdigit()and s[4].isdigit and s[5].isdigit():
+                return True
+             if s[2].isdigit() == False and s[3].isdigit()==False and s[4].isdigit and s[5].isdigit():
+                 return True
+             if s[2].isdigit() == False and s[3].isdigit()==False and s[4].isdigit==False and s[5].isdigit():
+                 return True
              else:
                 return False
 
