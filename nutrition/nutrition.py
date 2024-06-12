c22f67b4395a731162fd530d780a7af562fa667a
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-12 06:01:38 +0000

    Wed, Jun 12, 2024, 11:31 AM +05:30

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,4 +1,11 @@
 #creating a dictionary to store all the key value pairs.
 
 fruits = {"apple":130 , "avocado":50 , "banana":110 , "cantaloupe":110, "grapefruit":60 , "grapes":90 , "honeydrew melon" : 50 , "kiwifruit" : 90 , "lemon" : 15 ,
-          "lime": 20 , "nectarine": 60 , "orange" : }
+          "lime": 20 , "nectarine": 60 , "orange" : 80 , "pear":100 , "peach" : 60 , "pineapple":50 , "plums":70 , "strawberries" : 50 , "sweet cherries " : 100 , "tangerine" : 50 ,
+          "watermelon" : 80 }
+
+name = str(input("Item: " )).lower()
+
+if name in fruits:
+    print(fruits)
+
