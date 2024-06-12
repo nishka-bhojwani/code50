bd814e40ee0c8981b17b22ae2ddb488ca99c8a11
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-12 06:05:47 +0000

    Wed, Jun 12, 2024, 11:35 AM +05:30

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -4,8 +4,13 @@ fruits = {"apple":130 , "avocado":50 , "banana":110 , "cantaloupe":110, "grapefr
           "lime": 20 , "nectarine": 60 , "orange" : 80 , "pear":100 , "peach" : 60 , "pineapple":50 , "plums":70 , "strawberries" : 50 , "sweet cherries " : 100 , "tangerine" : 50 ,
           "watermelon" : 80 }
 
+#taking input from user as string i.e. the fruit
 name = str(input("Item: " )).lower()
 
+# checking whether the 
 if name in fruits:
-    print(fruits)
+    print(f"Calories : {fruits[name]}")
+else:
+    print("please enter a fruit")
+
 
