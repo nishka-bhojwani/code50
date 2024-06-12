c8eea728dfa91b6542eac4c70d31d07e5db5e3b3
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-12 05:50:54 +0000

    Wed, Jun 12, 2024, 11:20 AM +05:30

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,3 +1,3 @@
 #creating a dictionary to store all the key value pairs.
 
-fruits = {"apple":130 , "avocado":50 , "banana":110 , }
+fruits = {"apple":130 , "avocado":50 , "banana":110 , "cantaloupe":110, "grapefruit":60 , "grapes":90 , "honeydrew melon" }
