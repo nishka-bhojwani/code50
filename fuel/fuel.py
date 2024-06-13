b30003c67108ff4da29d6cbc5d61f552b5c1608c
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-13 08:09:19 +0000

    Thu, Jun 13, 2024, 1:39 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,9 +2,14 @@
 
 def main():
     user = str(input("Fraction: "))
-    new = user.split("/")\
-    new1 = int(new[0])
-    new2 = int(new[1])
+    new = user.split("/")
+    if len()
+    try:
+        new1 = int(new[0])
+        new2 = int(new[1])
+    except ValueError:
+        pass
+
 
     while (new1/new2 )> 1:
         user = str(input("Fraction: "))
