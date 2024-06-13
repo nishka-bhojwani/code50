3b3e278cf57cf572cd90bc6929df4fcb3eeb910e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-13 08:12:47 +0000

    Thu, Jun 13, 2024, 1:42 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,30 +3,21 @@
 def main():
     user = str(input("Fraction: "))
     new = user.split("/")
-    if len()
     try:
         new1 = int(new[0])
         new2 = int(new[1])
-    except ValueError:
-        pass
+    
 
 
-    while (new1/new2 )> 1:
+
+    while (new1/new2 )<0 or (new1/new2)>1:
         user = str(input("Fraction: "))
     print(f"{(new1/new2)*100}%")
 
 
 
 
-def second(y):
-    while True:
-        try:
-            return y
-            break
-        except ZeroDivisionError:
-            pass
-        except ValueError:
-            pass
+
 
 main()
 
