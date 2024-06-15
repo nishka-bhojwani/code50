accc347003ed533f10dddc06a1be9cfa63904e88
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-15 07:50:04 +0000

    Sat, Jun 15, 2024, 1:20 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,6 +5,32 @@ def main():
          fuel= str(input("Fraction: "))
          new = fuel.split("/")
 
+    #trying to check whether the inputed numerator(new1) and denominator(new2) are integers or not
+    try:
+         new1 = int(new[0])
+         new2 = int(new[1])
+
+    # calculating the fraction
+         fraction = new1/new2
+
+         if 0<fraction<1:
+            print(f"{fraction*100}%")
+            break
+         elif fraction==0:
+            print("E")
+            break
+         elif fraction == 1:
+            print("F")
+            break
+
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+
+
+
 
 
 main()
