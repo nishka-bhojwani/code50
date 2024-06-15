c934ad72e829506200e66b746048abb57886c737
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-15 05:44:17 +0000

    Sat, Jun 15, 2024, 11:14 AM +05:30

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ def main():
 
 
         try:
-            # Ensure both parts are integers
+            # Ensure that both the numerator and denominator are fractions 
             new1 = int(new[0])
             new2 = int(new[1])
-      
+
             # Calculate the fraction
             fraction = new1 / new2
 
-            # Ensure the fraction is within the valid range (0, 1]
+            # loop to check the range of fraction and print the given percentages based on the fraction
             if 0 < fraction < 1:
                 print(f"{fraction * 100:.0f}%")
                 break
