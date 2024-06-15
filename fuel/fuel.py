51b6d3f9c9c6d79ea313e86d6d4f3b7253b9dcf6
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-15 08:10:27 +0000

    Sat, Jun 15, 2024, 1:40 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -13,13 +13,13 @@ while True: #to create a loop to keep promting the user
     # calculating the fraction
          fraction = new1/new2
      #conditions to check the input fraction and print the o/p percentage
-         if 0<fraction<1 and (fraction*100)>=1 and (fraction*100)<=99:
+         if 0<fraction<1 and (fraction*100)>1 and (fraction*100)<99:
             print(f"{fraction*100:.0f}%")
             break # to exit the loop once
          elif fraction==0 or (fraction*100)<=1:
             print("E")
             break
-         elif fraction == 1 or (fraction*100)>=99:
+         elif fraction == 1 or (fraction*100)>=99 :
             print("F")
             break
 
