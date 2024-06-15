6b27a9919923fee8bfa4383331e6d9dbf0dcee81
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-15 10:02:56 +0000

    Sat, Jun 15, 2024, 3:32 PM +05:30

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,3 +1,4 @@
+
 menu = {
         "Baja Taco": 4.25,
         "Burrito": 7.50,
@@ -11,17 +12,17 @@ menu = {
         }
 
 try:
-    Total = 0
-    while True:
+    Total = 0 # creating a variable called total
+    while True: #while loop to keep prompting for item till user exits
         item = str(input("Item: ")).title()
         if item in menu:
-               new = menu[item]
+               new = menu[item] #cost of every item from menu
         else:
             continue
-        Total = Total+ new
-        print(f"${Total:.2f}")
+        Total = Total+ new #adding the cost of the item to the total
+        print(f"${Total:.2f}") #printing the total with two decimal places
 
-#exiting the programme a
+#exiting the programme also printing a new line.
 except EOFError:
     exit
     print(" ")
