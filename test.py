671d726a468e4eda9fd18607b68ea5cba57b3e10
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-15 05:41:27 +0000

    Sat, Jun 15, 2024, 11:11 AM +05:30

--- test.py
+++ test.py
@@ -13,20 +13,24 @@ def main():
             # Ensure both parts are integers
             new1 = int(new[0])
             new2 = int(new[1])
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
 
             # Calculate the fraction
             fraction = new1 / new2
 
             # Ensure the fraction is within the valid range (0, 1]
-            if 0 <= fraction <= 1:
+            if 0 < fraction < 1:
                 print(f"{fraction * 100:.0f}%")
                 break
-            
+            elif fraction == 0:
+                print("E")
+            elif fraction == 1:
+                print("F")
 
 
-        except ValueError:
-            pass
-        except ZeroDivisionError:
-            pass
+
 
 main()
