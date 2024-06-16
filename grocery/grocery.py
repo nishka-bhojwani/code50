a060ecfe8daed6045205a90744f8d9c97fce19d3
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-16 20:27:47 +0000

    Mon, Jun 17, 2024, 1:57 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,7 +1,7 @@
 # taking input from user
 try:
     grocery = {} #empty dictionary to store items and their quantity
-    while True: 
+    while True:
         item = str(input()).strip().upper()
         if item not in grocery:
             grocery[item]=1
@@ -11,11 +11,11 @@ try:
 except EOFError:
     exit
 
-
+#creating and sorting a list to obtain the keys in alphabetical order
 new= list(grocery.keys())
-new.sort()
+new.sort() #inbuilt function to sort a list
 
-for i in new:
+for i in new: #for the keys in sorted list printing the values and keys
     print(f"{grocery[i]} {i}")
 
 
