c31270345f6b2d1e240f2d883f45cb941cb11467
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-16 07:01:31 +0000

    Sun, Jun 16, 2024, 12:31 PM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -4,10 +4,16 @@ try:
     counter = 0
     while True:
         item = str(input())
-        
-
+        counter = counter+1
+        grocery.update({item:counter})
+        if item in grocery:
+            grocery[item] = counter+1
+        else:
+            grocery[item] = counter
+    print(grocery)
 
 
 except EOFError:
+    print("")
     exit
-    print(" ")
+    
