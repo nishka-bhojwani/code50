b32cc7956514c75d6ef393ee478e64d109430e9b
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-16 20:26:17 +0000

    Mon, Jun 17, 2024, 1:56 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,13 +1,13 @@
 # taking input from user
 try:
-    grocery = {}
-    while True:
+    grocery = {} #empty dictionary to store items and their quantity
+    while True: 
         item = str(input()).strip().upper()
         if item not in grocery:
             grocery[item]=1
         elif item in grocery:
             grocery[item]+=1
-
+#exits when user presses ctr-d
 except EOFError:
     exit
 
