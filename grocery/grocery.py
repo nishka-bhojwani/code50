ebc2328705b023b48471ce20bb6424a375a18711
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-16 12:16:41 +0000

    Sun, Jun 16, 2024, 5:46 PM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,14 +1,12 @@
 # taking input from user
 try:
     grocery = {}
-    counter = 0
     while True:
         item = str(input())
         if item not in grocery:
-            grocery.update({item:1})
+            grocery[item]=1
         elif item in grocery:
-            counter+=1
-            grocery.update({item:counter})
+            grocery[item]+=1
 
 
 
