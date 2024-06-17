329682957f2c7afb7b6345eed5519bd76ec2ca9e
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-17 06:29:50 +0000

    Mon, Jun 17, 2024, 11:59 AM +05:30

---
+++ outdated/outdated.py
@@ -0,0 +1,17 @@
+#taking specific input from user else reprompt
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
