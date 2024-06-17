2065240c98b2f8667e1d14b33da6d53cbb3f2772
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-17 19:44:30 +0000

    Tue, Jun 18, 2024, 1:14 AM +05:30

--- packages.py
+++ packages.py
@@ -1,3 +1,8 @@
 #pip is a programme that comes with python which allows you to import packages
 #PyPI - webpage eith all packages
 
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.cow("hello, " + sys.argv[1])
