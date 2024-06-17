9989c168bc90b522ffb8b09e0953c99efa91fd80
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-17 19:34:38 +0000

    Tue, Jun 18, 2024, 1:04 AM +05:30

--- cla.py
+++ cla.py
@@ -2,8 +2,7 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")
 
-print("hello, my name is,",sys.argv[1])
+for arg in 
+    print("hello, my name is,",sys.argv[1])
 
