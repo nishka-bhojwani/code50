c51cd294ba2b81fe1b42104302b1a795211bfef8
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-17 13:16:14 +0000

    Mon, Jun 17, 2024, 6:46 PM +05:30

--- libraries.py
+++ libraries.py
@@ -1,1 +1,8 @@
-#random.py has built in functions , like flipping a coin 
+#random.py has built in functions , like flipping a coin , picking a random number
+#import keyword allows you to import functions from a library
+#seq is like a list
+#problem to simulate a coin , 50% probability
+
+import random
+
+random.choice(["heads","tails"])
