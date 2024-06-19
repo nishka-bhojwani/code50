51ca88d62b0162244e240b0a66b374cf67f84ee9
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-19 12:11:25 +0000

    Wed, Jun 19, 2024, 5:41 PM +05:30

--- api.py
+++ api.py
@@ -16,4 +16,5 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+sys.argv[1])
-print(response.json())
+print(json.dumps(response.json(),indent =2 ))
+#json.dumps() from documentation - helps make it more readable
