f3ef16ae411932324c5ed908ad0ece8de857c582
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-19 13:33:37 +0000

    Wed, Jun 19, 2024, 7:03 PM +05:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,6 +1,6 @@
-import emoji
+import emoji #dowloading the emoji module using pip then importing it
 
-text = str(input("Input: "))
-new = emoji.emojize(text, language = 'alias')
-print(f"Output: {new}")
+text = str(input("Input: ")) #taking user input as string
+new = emoji.emojize(text, language = 'alias') #using the emojize function from documentation to convert it to an emoji
+print(f"Output: {new}") #printing o/p
 
