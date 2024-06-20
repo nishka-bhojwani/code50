7975443c745745afea5fd5f07f7420d37d606746
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 05:10:51 +0000

    Thu, Jun 20, 2024, 10:40 AM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,12 @@
 from pyfiglet import Figlet
 import sys
+from random import choice 
 
 figlet = Figlet()
 
 if len(sys.argv)==1 :
     text = str("Input :")
-    
+
 else:
     text = str(input("Input: "))
 
