80f51d4f321577f5ac2bd516d39293c7a5500d4c
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 05:07:16 +0000

    Thu, Jun 20, 2024, 10:37 AM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,8 +1,11 @@
 from pyfiglet import Figlet
 import sys
 
-if len(sys.argv) not in range(1,4):
-    sys.exit()
+figlet = Figlet()
+
+if len(sys.argv)==1 :
+    text = str("Input :")
+    
 else:
     text = str(input("Input: "))
 
