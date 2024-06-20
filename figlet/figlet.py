1e1f331a29a79aed4e3425049736373f7a585159
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 05:29:10 +0000

    Thu, Jun 20, 2024, 10:59 AM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,13 +1,13 @@
 from pyfiglet import Figlet
 import sys
-from random import choice
+from random import random
 
 figlet = Figlet()
 
 if len(sys.argv)==1 or len(sys.argv)==3 :
     text = str(input("Input :"))
     if len(sys.argv)==1:
-        f = choice(figlet.getFonts())
+        f = random(figlet.getFonts())
         figlet.setFont(font=f)
         print(f"Output: {figlet.renderText(text)}")
 
