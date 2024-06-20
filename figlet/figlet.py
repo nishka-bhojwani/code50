19febe8e15808e42a5913d8655faed4438be3884
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 05:45:22 +0000

    Thu, Jun 20, 2024, 11:15 AM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -13,9 +13,19 @@ if len(sys.argv)==1 or len(sys.argv)==3 :
     elif len(sys.argv)==3:
         if (sys.argv[1] == "-f" or sys.argv[1]=="--font") and sys.argv[2] in figlet.getFonts():
             figlet.setFont(font=sys.argv[2])
+        else:
+            print("Invalid usage")
+            sys.exit()
+
+    else:
+        print("Invalid usage")
+        sys.exit()
 
 
     print(f"Output: {figlet.renderText(text)}")
+else:
+    print("Invalid usage")
+    sys.exit()
 
 
 
