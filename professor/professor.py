bf3bc142db640947524fca74f34f6a06b3402606
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 12:03:21 +0000

    Thu, Jun 20, 2024, 5:33 PM +05:30

---
+++ professor/professor.py
@@ -0,0 +1,11 @@
+while True:
+
+    try:
+        level = int(input("Level: "))
+        if 4<=level<=0:
+            continue
+        if
+        else:
+            break
+    except ValueError:
+        continue
