300b50c7e2d40a80da157161c774251be2a7d6dc
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-20 10:09:09 +0000

    Thu, Jun 20, 2024, 3:39 PM +05:30

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,19 +1,19 @@
-
+#from documentation of inflect 
 import inflect
 
 p = inflect.engine()
 
-names=[]
-while True:
-    try:
+names=[] #creating an empty list called names
+while True: #reprompting for names
+    try: #trying to get names till ctrl d is pressed (EOF- error handling )
         name = str(input("Name: ")).strip().capitalize()
-        names.append(name)
-    except EOFError:
+        names.append(name) #appending names in a list called names
+    except EOFError: #breaking out of the while loop if ctrl d pressed
         break
     except ValueError:
         break
 
-new_name = p.join(names)
+new_name = p.join(names) #using the method join the names
 print(f"\nAdieu, adieu, to {new_name}")
 
 
