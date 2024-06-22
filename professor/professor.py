9c8c6b50deb680b25df2187b2205a5e9a4584846
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-06-22 05:42:28 +0000

    Sat, Jun 22, 2024, 11:12 AM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -1,4 +1,4 @@
-"""while True:
+while True:
 
     try:
         level = int(input("Level: "))
@@ -8,23 +8,4 @@
             break
     except ValueError:
         continue
-
-"""
-
-import random
-
-
-def main():
-    ...
-
-
-def get_level():
-    ...
-
-
-def generate_integer(level):
-    ...
-
-
-if __name__ == "__main__":
-    main()
+    
