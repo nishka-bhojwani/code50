3544f5a52977c97b2a653cae2364e13780aa796a
nishka-bhojwani <103505256+nishka-bhojwani@users.noreply.github.com>
2024-07-09 09:50:17 +0000

    Tue, Jul 9, 2024, 3:20 PM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -13,62 +13,62 @@
 
 import random
 
-
 def main():
     level = get_level()
-    generate_integer(level)
+    counter, correct = ask_questions(level, 10)
+    print(f"{correct}")
 
-
-
-
+ #getting a level , only accepts 1,2,3
 def get_level():
     while True:
-
         try:
             level = int(input("Level: "))
-            if 1<=level<=3:
+            if 1 <= level <= 3:
                 return level
+
             else:
                 continue
         except ValueError:
             pass
 
+#generating integers based on the level
 
 def generate_integer(level):
     if level == 1:
-        return random.randint(0,9)
+        return random.randint(0, 10), random.randint(0, 10)
     elif level == 2:
-        return random.randint(0,99)
-
+        return random.randint(0, 100), random.randint(0, 100)
     elif level == 3:
-       return random.randint(0,999)
+        return random.randint(0, 1000), random.randint(0, 1000)
 
+#keeping count of correct anwers and number of questions answered
+def ask_questions(level, num_questions):
+    counter = 0
+    correct = 0
 
-def solve(X,Y):
-     X= generate_integer(get_level())
-     Y= generate_integer(get_level())
-     correct_answer = X +Y
-     tries  = 0
-     while tries <3:
-         answer = input(f"{X} + {Y} =" )
-         try:
-            answer1 = int(answer)
-            if answer1 == correct_answer:
-                return True
-            else:
-                print("EEE")
-            tries+=1
-         except ValueError:
-            print("EEE")
-            tries+=1
+    for _ in range(num_questions):
+        X, Y = generate_integer(level) #generating X and Y based on level
+        correct_answer = X + Y
+        attempts = 0
 
-            print(correct_answer)
-            
+        while attempts < 3: #prints EEE till 3 attempts then prints the answer
+            try:
+                answer = int(input(f"{X} + {Y} = "))
+                if answer == correct_answer:
+                    correct += 1
+                    break
+                else:
+                    print("EEE")
+                    attempts += 1
+            except ValueError:
+                attempts += 1
 
+        if attempts == 3: #prints correct answer after 3 attempts
+            print(f"{correct_answer}.")
 
+        counter += 1
 
-
-
+    return counter, correct #returns the answers given correctly and the number of questions asked
 
 if __name__ == "__main__":
     main()
